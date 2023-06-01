3c1b3968b66cddd7d39491eb7793d2ababcd7b00
ourfalia <>
2023-06-01 08:21:57 +0000

    add reservation model

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Table
+from .models import Table, Reservation
 
 
 admin.site.register(Table)
+admin.site.register(Reservation)
--- reservation/models.py
+++ reservation/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -9,3 +10,13 @@ class Table(models.Model):
 
     def __str__(self):
         return f'Table number {self.number} is for  {self.capacity} people'
+
+
+class Reservation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    booking_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} has booked table numer {self.table.number} on {self.booking_time}'
