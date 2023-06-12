3e5c7e3e04f3c6b05a0c4da95e07acb529e6cad3
ourfalia <>
2023-06-12 02:31:07 +0000

    add static and media files

--- arabista/settings.py
+++ arabista/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'home',
     'reservation',
+    'menu',
 ]
 
 MIDDLEWARE = [
@@ -129,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
