fef361d322823de7d77450c5415608493f9a3759
ourfalia <>
2023-06-20 22:15:06 +0000

    update base.html and add links

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def profile_view(request):
     """ A view to return the index page """
     profile = get_object_or_404(UserProfile, user=request.user)
