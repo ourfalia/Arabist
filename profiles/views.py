ec2980490d08ad30f010930071116209a62ce8a4
ourfalia <>
2023-06-20 16:23:52 +0000

    update availability_form, reservation_list, profile.html and edit_reservation files

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 # Create your views here.
 
 
 def profile_view(request):
     """ A view to return the index page """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
-    return render(request, 'profiles/profile.html')
+    template = 'profiles/profile.html'
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, template, context)
